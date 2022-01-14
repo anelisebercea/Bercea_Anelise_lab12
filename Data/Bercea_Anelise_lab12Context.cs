b987191dd9681a8c07b8f1a64627525c87a8c046
NICOLETA-ANELISE BERCEA <nicoleta.bercea@stud.ubbcluj.ro>
2022-01-14 12:29:21 +0200

    Add project files.

---
+++ Data/Bercea_Anelise_lab12Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Bercea_Anelise_lab12.Models;
+
+namespace Bercea_Anelise_lab12.Data
+{
+    public class Bercea_Anelise_lab12Context : DbContext
+    {
+        public Bercea_Anelise_lab12Context (DbContextOptions<Bercea_Anelise_lab12Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Bercea_Anelise_lab12.Models.ShopList> ShopList { get; set; }
+    }
+}
